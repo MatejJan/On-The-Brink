7acaa89f3caa2bb19e54ede92bd6ad8bda902666
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2021-01-30 08:52:32 +0100

    FoundCount and buttons tor try out the methods of the inventory.
    Made a property that gets the number of found items. Made buttons to try out finding items/adding them to the inventory and applying recipes.

--- On the Brink/Assets/Scripts/Inventory.cs
+++ On the Brink/Assets/Scripts/Inventory.cs
@@ -6,13 +6,34 @@ using UnityEngine;
 // Information stored about all collectible items. If it has been found at least ones and how many there is in the inventory.
 public class ItemData
 {
-    public bool found = false;
-    public int count = 0;
+    public bool Found { get; set; }
+    public int Count { get; set; }
 }
 
 public class Inventory : MonoBehaviour
 {
     Dictionary<GameObject, ItemData> inventoryItems = new Dictionary<GameObject, ItemData>();
+
+    // Gets the number of found collectible items.
+    public int FoundCount
+    {
+        get
+        {
+            int count = 0;
+
+            // Goes through the inventory dictionary and count how many items are found.
+            foreach (KeyValuePair<GameObject, ItemData> item in inventoryItems)
+            {
+                if (item.Value.Found)
+                {
+                    count++;
+                }
+            }
+
+            // Returns the number of found items.
+            return count;
+        }
+    }
 
     // Start is called before the first frame update.
     void Start()
@@ -27,16 +48,28 @@ public class Inventory : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        // Debug messages to see that everything works.
+        Debug.Log("Found " + FoundCount);
+
+        foreach (KeyValuePair<GameObject, ItemData> item in inventoryItems)
+        {
+            Debug.Log(item.Key.name + " count: " + item.Value.Count);
+        }
+
+    }
+
     // Add item to inventory and mark it as found.
-    void AddItem(GameObject itemType)
+    public void AddItem(GameObject itemType)
     {
         var itemData = inventoryItems[itemType];
-        itemData.found = true;
-        itemData.count++;
+        itemData.Found = true;
+        itemData.Count++;
     }
 
     // Remove and add the correct items after aplying a recipe.
-    void ApplyRecipe(GameObject recipeVariant)
+    public void ApplyRecipe(GameObject recipeVariant)
     {
         // Get ingredients (the two items we are combining) and the resulting item from the recipe prefab.
         Recipe recipeVariantScript = recipeVariant.GetComponent<Recipe>();
@@ -50,9 +83,9 @@ public class Inventory : MonoBehaviour
     }
 
     // Removing item from inventory.
-    void RemoveItem(GameObject itemType)
+    public void RemoveItem(GameObject itemType)
     {
         var itemData = inventoryItems[itemType];
-        itemData.count--;
+        itemData.Count--;
     }
 }
